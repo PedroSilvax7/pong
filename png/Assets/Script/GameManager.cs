ec4f5884cdbc14209954b0d1202565c814ee6e6b
PedroSilvax7 <145567161+PedroSilvax7@users.noreply.github.com>
2025-10-14 08:31:11 -0300

    fhyfuiygfud

--- png/Assets/Script/GameManager.cs
+++ png/Assets/Script/GameManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -6,25 +8,46 @@ public class GameManager : MonoBehaviour
     public int pontuacaoDoJogador1;
 
     public int pontuacaoDoJogador2;
+
+    public Text textoDePontuacao;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        pontuacaoDoJogador1 = 0;
+        pontuacaoDoJogador2 = 0;
+        textoDePontuacao.text = pontuacaoDoJogador1 + " X " + pontuacaoDoJogador2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.R)){
+            ReiniciarPartida();
+        }
     }
 
     public void AumentarPontuacaoDoJogador1(){
         pontuacaoDoJogador1 += 1;
+        AtualizarTextoDePontuaçao();
 
     }
 
     public void AumentarPontuacaoDoJogador2(){
         pontuacaoDoJogador2 +=1;
+        AtualizarTextoDePontuaçao();
 
+    }
+
+    public void AtualizarTextoDePontuaçao(){
+        textoDePontuacao.text = pontuacaoDoJogador1 + " X " + pontuacaoDoJogador2;
+    }
+
+    private void ReiniciarPartida(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+    private void SairDoJogo(){
+        Application.Quit();
     }
 }
